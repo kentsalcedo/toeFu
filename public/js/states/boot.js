8fcb8d7c62e5e4d4c015b96bd7c251049e771aa1
Kent Salcedo <kentsalcedo@gmail.com>
2016-01-23 11:56:24 -1000

    Added asset autoloader for assets

--- public/js/states/boot.js
+++ public/js/states/boot.js
@@ -8,6 +8,21 @@ ToeFu.Boot = function (){
 
 ToeFu.Boot.prototype.preload = function() {
   // will preload assets
+  // want to actually outload
+  // ToeFu.game.load.spritesheet('player','assets/wahtever.png',12, 324,12)
+
+  //autoloads assets
+  Object.keys(ToeFu.ASSETS).forEach(function(type){
+    for( var asset in ToeFu.ASSETS[type]){
+      ToeFu.game.load[type.toLowerCase()](
+        ToeFu.ASSETS[type][asset].name,
+        ToeFu.ASSETS[type][asset].path,
+        ToeFu.ASSETS[type][asset].width,
+        ToeFu.ASSETS[type][asset].height,
+        ToeFu.ASSETS[type][asset].frames
+      );
+    }
+  });
 };
 
 ToeFu.Boot.prototype.create = function() {
